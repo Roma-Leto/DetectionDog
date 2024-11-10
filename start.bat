f56d1a68008a3e0c85988c358e442aae54664166
tessaract <tessaract@mail.com>
2024-11-10 20:43:26 +0300

    Убраны лишние для контроля версии файлы и папки.

--- start.bat
+++
@@ -1,3 +0,0 @@
-CALL C:\PythonProjects\DjangoProject\venv\Scripts\activate
-C:\PythonProjects\DjangoProject\winenv\Scripts\python C:\PythonProjects\DjangoProject\manage.py runserver 0.0.0.0:8004
-pause
